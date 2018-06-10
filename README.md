5233e9c77b92ad7c07f1cca9078f46f4c5ed0394
Harald Lapp <harald@octris.org>
2018-06-10 19:13:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# perspective
+
+Perspective projection of images.
+
+Example images: A15374911, A21192638, A43295079
---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CPPFLAGS="-I/usr/local/include/opencv -I/usr/local/include/opencv2"
+CXXFLAGS="--std=c++1z -stdlib=libc++"
+LDFLAGS="-L/usr/local/lib -lopencv_core -lopencv_imgproc -lopencv_imgcodecs -lopencv_calib3d"
+
+g++ $CPPFLAGS $CXXFLAGS $LDFLAGS main.cpp

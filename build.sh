763ce9484c1a8dd391fc74079a26c9cf8738ea1a
Harald Lapp <h.lapp@clipdealer.de>
2019-07-25 18:27:23 +0200

    cleaned up build-script

--- build.sh
+++ build.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-if [[ "$1" = "" ]]; then
-    echo "usage: build.sh <cpp-file>"
-    exit 1
-fi
-
-dir=$(dirname "$1")
-base=$(basename "$1" ".cpp")
-
 CPPFLAGS="-I/usr/local/include/opencv -I/usr/local/include/opencv2"
 CXXFLAGS="--std=c++1z -stdlib=libc++"
 LDFLAGS="-L/usr/local/lib -lopencv_core -lopencv_imgproc -lopencv_imgcodecs -lopencv_calib3d"
 
-g++ $CPPFLAGS $CXXFLAGS $LDFLAGS -o "$dir/$base" "$dir/$base.cpp"
+g++ $CPPFLAGS $CXXFLAGS $LDFLAGS -o scene scene.cpp

d90e6615207cd3368114a78fef253f860942ea4b
Harald Lapp <h.lapp@clipdealer.de>
2022-01-19 19:54:33 +0100

    removed obsolete file

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CPPFLAGS="-I/usr/local/include/opencv -I/usr/local/include/opencv2"
-CXXFLAGS="--std=c++1z -stdlib=libc++"
-LDFLAGS="-L/usr/local/lib -lopencv_core -lopencv_imgproc -lopencv_imgcodecs -lopencv_calib3d"
-
-g++ $CPPFLAGS $CXXFLAGS $LDFLAGS -o scene scene.cpp

e7d3aca8db749a89f7bc6878548fd71612233ee7
Harald Lapp <h.lapp@clipdealer.de>
2022-01-19 19:55:51 +0100

    initial commit xmake script

---
+++ xmake.lua
@@ -0,0 +1,8 @@
+add_rules("mode.debug", "mode.release")
+add_requires("CONAN::nlohmann_json/3.10.5", { alias = "json" })
+add_requires("opencv 4.2.0", { alias = "opencv", configs = { shared = false, gtk = false } })
+
+target("scene")
+    set_kind("binary")
+    add_files("src/*.cpp")
+    add_packages("opencv", "json")
